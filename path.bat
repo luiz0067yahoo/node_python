8ff71558b8557059629f0fa377bb2c006ef05759
@luiz0067yahoo <102493028+luiz0067yahoo@users.noreply.github.com>
2024-09-26 15:20:33 -0300

    Update path.bat

--- path.bat
+++ path.bat
@@ -43,4 +43,5 @@ Write-Output "Navegando para a pasta 'react': $reactPath"
 # node 16
 #npm uninstall -g expo-cli 
 #npm install -g expo-cli@4.12.0
+#npm install expo@44.0.0
 #npx create-expo-app aula06
