091c884c34d48bb56d089dd21e457b4bbf41a9f2
@luiz0067yahoo <102493028+luiz0067yahoo@users.noreply.github.com>
2024-09-26 12:22:53 -0300

    Update path.bat

--- path.bat
+++ path.bat
@@ -1,16 +1,25 @@
 # Definindo os caminhos
 $nodeDirectory = "C:\node\node-v20.17.0-win-x64"
 $androidStudioDirectory = "C:\android-studio\bin"
+$nodeJsPath = "C:\Program Files\nodejs"  # Caminho da pasta do Node.js a ser removida
+
+# Remove a pasta do Node.js, se existir
+if (Test-Path -Path $nodeJsPath) {
+    Remove-Item -Path $nodeJsPath -Recurse -Force
+    Write-Output "Pasta 'C:\Program Files\nodejs' removida."
+} else {
+    Write-Output "Nenhuma pasta 'C:\Program Files\nodejs' encontrada para remoção."
+}
 
 # Remove o caminho do Node.js do PATH existente, se presente
-$newPath = ($env:PATH -split ';' | Where-Object { $_ -ne 'C:\Program Files\nodejs\' -and $_ -ne 'C:\Program Files\nodejs' }) -join ';'
+$newPath = ($env:PATH -split ';' | Where-Object { $_ -ne $nodeJsPath }) -join ';'
 $newPath = ($newPath -split ';' | Where-Object { $_ -ne $nodeDirectory }) -join ';'
-
-# Remove o caminho do Android Studio do PATH existente, se presente
-$newPath = ($newPath -split ';' | Where-Object { $_ -ne 'C:\android-studio\bin' }) -join ';'
 
 # Adiciona o caminho do Node.js
 $newPath += ";$nodeDirectory"
+
+# Remove o caminho do Android Studio do PATH existente, se presente
+$newPath = ($newPath -split ';' | Where-Object { $_ -ne 'C:\android-studio\bin' }) -join ';'
 
 # Adiciona o caminho do Android Studio ao PATH
 $newPath += ";$androidStudioDirectory"
@@ -27,8 +36,8 @@ setx PATH $newPath
 
 # Definindo aliases
 Set-Alias node "C:\node\node-v20.17.0-win-x64\node.exe"
-Set-Alias npm "C:\node\node-v20.17.0-win-x64\npm.exe"
-Set-Alias npx "C:\node\node-v20.17.0-win-x64\npx.exe"
+Set-Alias npm "C:\node\node-v20.17.0-win-x64\npm.cmd"
+Set-Alias npx "C:\node\node-v20.17.0-win-x64\npx.cmd"
 
 # Definindo o caminho do diretório
 $reactPath = "$env:USERPROFILE\Documents\react"
@@ -45,5 +54,7 @@ if (-not (Test-Path -Path $reactPath)) {
 Set-Location -Path $reactPath
 Write-Output "Navegando para a pasta 'react': $reactPath"
 
+}
+
 # Criar um novo projeto Expo
-#npx expo init $projectFolder --template blank
+# npx expo init $projectFolder --template blank
